dd29b957c2f8b72db850da3c142b5866420b91e5
Meet <meetbhalodiya1030@gmail.com>
2022-04-13 22:40:40 +0530

    card created

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+
+const url = "https://randomuser.me/api/";
+const App = () => {
+	const [details, setDetails] = useState("");
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+
+	const fetchData = async () => {
+		try {
+			const resp = await fetch(url);
+			const data = await resp.json();
+			setDetails(data);
+			setLoading(false);
+		} catch {
+			setLoading(false);
+			setError(true);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const { results, info } = details;
+	console.log(results);
+	return (
+		<>
+			{loading && <h1 className='status'>Loading...</h1>}
+			{error && (
+				<h1 className='status'>Error... Please try refreshing the page</h1>
+			)}
+
+			{loading || (
+				<>
+					<h1 className='heading'>Bonjour XXX</h1>
+					<article className='page-container'>
+						<section className='profile-info'>
+							{results.map((value) => {
+								return (
+									<div key={value.phone}>
+										<header className='profile-header'>
+											<div>
+												<img
+													className='profile'
+													src={value.picture.thumbnail}
+													alt='profile'
+												/>
+												<h3 className='name'>{`${value.name.title} ${value.name.first} ${value.name.last}`}</h3>
+												<p className='email'>{value.email}</p>
+											</div>
+											<button className='btn'>Editer Mon Profil</button>
+										</header>
+										<main className='main-content'>
+											<p className='info'>INFORMATION</p>
+											<p>Age: {value.dob.age}</p>
+											<p>
+												Addresse:
+												{` ${value.location.street.number} ${value.location.street.name} ${value.location.city} ${value.location.state} ${value.location.country} `}
+											</p>
+											<p>Telephone:{value.phone}</p>
+										</main>
+									</div>
+								);
+							})}
+						</section>
+						<aside className='sideInfo'>
+							<section className='etape1'>
+								<span className='num'>1</span>
+								<div className='etape'>
+									<span>Etape 1</span>
+									<p className='tag tag1'>En cours</p>
+								</div>
+							</section>
+							<section className='etape2'>
+								<span className='num'>2</span>
+								<div className='etape'>
+									<span>Etape 1</span>
+									<p className='tag'>En cours</p>
+								</div>
+							</section>
+							<section className='etape3'>
+								<span className='num'>3</span>
+								<div className='etape'>
+									<span>Etape 1</span>
+									<p className='tag'>En cours</p>
+								</div>
+							</section>
+						</aside>
+						<section className='footer'>
+							<h3>Auxerunt haec vulgi</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Laboriosam optio qui nihil rerum velit ullam distinctio,
+								deleniti vel, ducimus quo ut? Animi sapiente sit eaque inventore
+								qui corrupti iste adipisci.
+							</p>
+						</section>
+					</article>
+				</>
+			)}
+		</>
+	);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import reactDom from "react-dom";
-import People from "./people";
+import App from "./App";
 import "./index.css";
 
 function BirthdayList() {
-  return (
-    <article className="wrapper">
-      <People />
-    </article>
-  );
+	return (
+		<article className='wrapper'>
+			<App />
+		</article>
+	);
 }
 
 reactDom.render(<BirthdayList />, document.getElementById("root"));
